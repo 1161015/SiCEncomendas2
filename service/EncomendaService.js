5d2b831cbb2cabf0aa6a9744385947f1e745e56c
1161015 <1161015@isep.ipp.pt>
2018-12-10 15:09:04 +0000

    .

---
+++ service/EncomendaService.js
@@ -0,0 +1,83 @@
+const Item = require('../models/Item');
+
+
+	//constructor() { }
+
+	this.validacaoObrigatoriedade=function(idItemPai) {
+		const pedidoHTTP = require('request');
+		let urlPost = "https://sicarqsi2018.azurewebsites.net/api/produto/obrigatoriedade/";
+		let idsFilhos = [];
+		let i;
+		Item.findById(idItemPai, function (err, itemPai) {
+			Item.findAll({ itemPai: idItemPai }, function (err, ItensFilhos) {
+				ItensFilhos.forEach(element => {
+					idsFilhos.push(element.idProduto);
+				})
+			})
+			let args = {
+				data: {
+					"ProdutoPaiId": itemPai,
+					"ProdutosFilhosIds": idsFilhos
+				},
+				headers: { "Content-Type": "application/json" }
+			}
+			return new Promise(function (resolve, reject) {
+				pedidoHTTP.post(urlPost, args, function (data, response) {
+					if (data) return resolve();
+					return reject();
+				});
+			});
+		});
+	};
+
+
+	this.validarTaxaOcupacao=function(idItemPai) {
+		const pedidoHTTP = require('request');
+		let urlPost = "https://sicarqsi2018.azurewebsites.net/api/produto/verificarTaxaOcupacao/";
+
+		let idsFilhos = [];
+		let i;
+		Item.findById(idItemPai, function (err, itemPai) {
+			Item.findAll({ itemPai: idItemPai }, function (err, ItensFilhos) {
+				ItensFilhos.forEach(element => {
+					idsFilhos.push(element.idProduto);
+				})
+			})
+			let volume = calcularVolume(idsFilhos);
+			let args = {
+				data: {
+					"ProdutoPaiId": itemPai,
+					"ProdutosFilhosIds": idsFilhos,
+					"Volume": volume
+				},
+				headers: { "Content-Type": "application/json" }
+			}
+			return new Promise(function (resolve, reject) {
+				pedidoHTTP.post(urlPost, args, function (data, response) {
+					if (data) return resolve();
+					return reject();
+				});
+			});
+		});
+	}
+	
+
+	this.calcularVolume=function(idsFilhos) {
+		var i = 0;
+		var altura = 0;
+		var largura = 0;
+		var profundidade = 0;
+		var volume = 0;
+		var volumeTotal = 0;
+
+		idsFilhos.forEach(element => {
+			altura = element.altura;
+			largura = element.largura;
+			profundidade = element.profundidade;
+			volume = altura * largura * profundidade;
+			volumeTotal += volume;
+		});
+		return volumeTotal;
+	}
+
+//module.exports =  EncomendaService();
